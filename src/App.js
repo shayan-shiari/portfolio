13d1db89c588cc6375bdbe257be146a72b7a6b37
Shayan Shiari <shayanshiari@gmail.com>
2023-06-23 14:28:19 +0430

    cursor disabled

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/login/Login";
 import Posts from "./pages/posts/Posts";
-import Cursor from "./components/cursor/Cursor";
+// import Cursor from "./components/cursor/Cursor";
 
 const App = () => {
   return (
     <>
-      <Cursor />
+      {/* <Cursor /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
